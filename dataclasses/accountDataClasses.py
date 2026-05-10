f3bd64235f687858a9ba5e770f3c1170f38b9494
Joshua Boyd <boyd.jc.github@gmail.com>
2026-05-10 00:20:32 -0400

    added more dataclasses and enums for account api

--- dataclasses/accountDataClasses.py
+++ dataclasses/accountDataClasses.py
@@ -3,45 +3,17 @@ from enum import Enum
 
 
 """"
-TODOS
+TODOS (Also go through and set which variables are optional and which are not)
 ===============
-Account
-DateParam
-Order
-OrderRequest
-PreviewOrder
-OrderActivity
-ExecutionLeg
-Position
-ServiceError
-OrderLegCollection
-SecuritiesAccount
-SecuritiesAccountBase
-MarginAccount
-MarginInitialBalance
-MarginBalance
-CashAccount
-CashInitialBalance
-CashBalance
-TransactionBaseInstrument
-AccountsBaseInstrument
-AccountsInstrument
 TransactionInstrument
-TransationCashEquivalent
 CollectiveInvestment
-Currency
 TransactionEquity
 TransactionFixedIncome
-Forex
 Future
 Index
 TransactionMutualFund
 TransactionOption
 Product
-AccountFixedIncome
-AccountMutualFund
-AccountOption
-AccountAPIOptionDeliverable
 TransactionAPIOptionDeliverable
 APIOrderStatus
 TransactionType
@@ -172,6 +144,390 @@ class AccountEquity():
     description: str
     instrumentId: str
     netChange: float
+
+@dataclass
+class AccountFixedIncome():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+    maturityDate: str
+    factor: float
+    variableRate: float
+
+@dataclass
+class AccountMutualFund():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+
+@dataclass
+class AccountAPIOptionDeliverable():
+    symbol: str
+    deliverableUnits: float
+    apiCurrencyType: APICurrencyType
+    assetType: AssetType
+
+@dataclass
+class AccountOption():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+    optionDeliverables: list[AccountAPIOptionDeliverable]
+    putCall: PutCall
+    optionMultiplier: int
+    type: AccountOptionType
+    underlyingSymbol: str
+
+@dataclass
+class AccountsInstrument():
+    accountCashEquivalent: AccountCashEquivalent
+    accountEquity: AccountEquity
+    accountFixedIncome: AccountFixedIncome
+    accountOption: AccountOption
+
+@dataclass
+class Position():
+    shortQuantity: float
+    averagePrice: float
+    currentDayProfitLoss: float
+    currentDayProfitLossPercentage: float
+    longQuantity: float
+    settledLongQuantity: float
+    settledShortQuantity: float
+    agedQuantity: float
+    instrument: AccountsInstrument
+    marketValue: float
+    maintenanceRequirement: float
+    averageLongPrice: float
+    averageShortPrice: float
+    taxLotAverageLongPrice: float
+    taxLotAverageShortPrice: float
+    longOpenProfitLoss: float
+    shortOpenProfitLoss: float
+    previousSessionLongQuantity: float
+    previousSessionShortQuantity: float
+    currentDayCost: float
+    
+@dataclass 
+class CashAccount():
+    type: CashAccountType
+    accountNumber: str
+    roundTrips: int
+    isDayTrader: bool
+    isClosingOnlyRestricted: bool
+    pfcbFlag: bool
+    positions: list[Position]
+    initialBalances: CashInitialBalance
+    currentBalances: CashBalance
+    projectedBalances: CashBalance
+
+@dataclass
+class CashInitialBalance():
+    accruedInterest: float
+    cashAvaiableForTrading: float
+    cashAvaiableForWithdrawal: float
+    cashBalance: float
+    bondValue: float
+    cashReceipts: float
+    liquidationValue: float
+    longOptionMarketValue: float
+    longStockValue: float
+    moneyMarketFund: float
+    mutualFundValue: float
+    shortOptionMarketValue: float
+    shortStockValue: float
+    isInCall: float
+    unsettledCash: float
+    cashDebtCallValue: float
+    pendingDeposits: float
+    accountValue: float
+
+@dataclass
+class CashBalance():
+    cashAvaiableForTrading: float
+    cashAvaiableForWithdrawal: float
+    cashCall: float
+    longNonMarginableMarketValue: float
+    totalCash: float
+    cashDebitCallValue: float
+    unsettledCash: float
+
+@dataclass
+class MarginInitialBalance():
+    accruedInterest: float
+    avaiableFundsNonMarginableTrade: float
+    bondValue: float
+    buyingPower: float
+    cashBalance: float
+    cashAvaiableForTrading: float
+    cashReceipts: float
+    dayTradingBuyingPower: float
+    dayTradingBuyingPowerCall: float
+    dayTradingEquityCall: float
+    equity: float
+    equityPercentage: float
+    liquidationValue: float
+    longMarginValue: float
+    longOptionMarketValue: float
+    longStockValue: float
+    maintenanceCall: float
+    maintenanceRequirement: float
+    margin: float
+    marginEquity: float
+    moneyMarketFund: float
+    mutualFundValue: float
+    regTCall: float
+    shortMarginValue: float
+    shortOptionMarketValue: float
+    shortStockValue: float
+    totalCash: float
+    isInCall: float
+    unsettledCash: float
+    pendingDeposits: float
+    marginBalance: float
+    shortBalance: float
+    accountValue: float
+
+@dataclass
+class MarginBalance():
+    avaiableFunds: float
+    avaiableFundsNonMarginableTrade: float
+    buyingPower: float
+    buyingPowerNonMarginableTrade: float
+    dayTradingBuyingPower: float
+    dayTradingBuyingPowerCall: float
+    equity: float
+    equityPercentage: float
+    longMarginValue: float
+    maintenanceCall: float
+    maintenanceRequirement: float
+    marginBalance: float
+    regTCall: float
+    shortBalance: float
+    shortMarginValue: float
+    sma: float
+    isInCall: float
+    stockBuyingPower: float
+    optionBuyingPower: float
+
+@dataclass
+class MarginAccount():
+    type: MarginAccountType
+    accountNumber: str
+    roundTrips: int
+    isDayTrader: bool
+    isClosingOnlyRestricted: bool
+    pfcbFlag: bool
+    positions: list[Position]
+    initialBalances: MarginInitialBalance
+    currentBalance: MarginBalance
+    projectedBalance: MarginBalance
+
+@dataclass
+class SecuritiesAccount():
+    marginAccount: MarginAccount
+    cashAccount: CashAccount
+
+@dataclass
+class Account():
+    securitiesAccount: SecuritiesAccount
+
+@dataclass
+class DateParam():
+    date: str # format is yyyy-MM-dd'T'HH:mm:ss.SSZ
+
+@dataclass
+class Order():
+    session: Session
+    duration: Duration
+    orderType: OrderType
+    cancelTime: str
+    complexOrderStrategyType: ComplexOrderStrategyType
+    quantity: float
+    filledQuantity: float
+    remainingQuantity: float
+    requestedDestination: RequestedDestination
+    releaseTime: str
+    stopPrice: float
+    stopPriceLinkBasis: StopPriceLinkBasis
+    stopPriceLinkType: StopPriceLinkType
+    stopPriceOffset: float
+    stopType: StopType
+    priceLinkBasis: PriceLinkBasis
+    priceLinkType: PriceLinkType
+    price: float
+    taxLotMethod: TaxLotMethod
+    orderLegCollection: list[OrderLegCollection]
+    activationPrice: float
+    specialInstruction: SpecialInstruction
+    orderStrategyType: OrderStrategyType
+    orderId: int
+    cancelable: bool
+    editable: bool
+    status: Status
+    enteredTime: str
+    closeTime: str
+    tag: str
+    accountNumber: int
+    orderActivityCollection: list[OrderActivity]
+    replacingOrderCollection: list[Order]
+    childOrderStrategies: list[Order]
+    statusDescription: str
+    
+@dataclass
+class ExecutionLeg():
+    legId: int
+    price: float
+    quantity: float
+    mismarkedQuantity: float
+    instrumentId: int
+    time: str
+
+@dataclass
+class OrderActivity():
+    activityType: ActivityType
+    executionType: ExecutionType
+    quantity: float
+    orderRemainingQuantity: float
+    executionLegs: list[ExecutionLeg]
+
+@dataclass
+class OrderRequest():
+    session: Session
+    duration: Duration
+    orderType: OrderTypeRequest
+    cancelTime: str
+    complexOrderStrategyType: ComplexOrderStrategyType
+    quantity: float
+    filledQuantity: float
+    remainingQuantity: float
+    destinationLinkName: str
+    releaseTime: str
+    stopPrice: float
+    stopPriceLinkBasis: StopPriceLinkBasis
+    stopPriceLinkType: StopPriceLinkType
+    stopPriceOffset: float
+    stopType: StopType
+    priceLinkBasis: PriceLinkBasis
+    priceLinkType: PriceLinkType
+    price: float
+    taxLotMethod: TaxLotMethod
+    orderLegCollection: list[OrderLegCollection]
+    activationPrice: float
+    specialInstruction: SpecialInstruction
+    orderStrategyType: OrderStrategyType
+    orderId: int
+    cancelable: bool
+    editable: bool
+    status: Status
+    enteredTime: str
+    closeTime: str
+    accountNumber: int
+    orderActivityCollection: list[OrderActivity]
+    replacingOrderCollection: list[OrderStrategy]
+    childOrderStrategies: list[OrderStrategy]
+    statusDescription: str
+
+@dataclass
+class PreviewOrder():
+    orderId: int
+    orderStrategy: OrderStrategy
+    orderValidationResult: OrderValidationResult
+    commissionAndFee: CommissionAndFee
+
+@dataclass
+class OrderLegCollection():
+    orderLegType: OrderLegType
+    legId: int
+    instrument: AccountsInstrument
+    instruction: Instruction
+    positionEffect: PositionEffect
+    quantity: float
+    quantityType: QuantityType
+    divCapGains: DivCapGains
+    toSymbol: str
+
+@dataclass
+class SecuritiesAccountBase():
+    type: SecuritiesAccountBaseType
+    accountNumber: str
+    roundTrips: int
+    isDayTrader: bool
+    isClosingOnlyRestricted: bool
+    pfcbFlag: bool
+    position: list[Position]
+
+@dataclass
+class TransactionBaseInstrument():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+
+@dataclass
+class AccountsBaseInstrument():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+
+@dataclass
+class Currency():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+
+@dataclass
+class Forex():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netChange: float
+    type: ForexType
+    baseCurrency: Currency
+    counterCurrency: Currency
+
+@dataclass
+class Future():
+    activeContract: bool
+    type: FutureType
+    expirationDate: str
+    lastTradingDate: str
+    firstNoticeDate: str
+    multipler: float
+    transactionCashEquivalent: TransactionCashEquivalent
+    collectiveInvestment
+
+@dataclass
+class TransactionCashEquivalent():
+    assetType: AssetType
+    cusip: str
+    symbol: str
+    description: str
+    instrumentId: int
+    netchange: float
+    type: TransactionCashEquivalentType
+    
+
+
 
 
 
@@ -440,10 +796,79 @@ class MarginAccountType(str, Enum):
     CASH = "CASH"
     MARGIN = "MARGIN"
 
+class CashAccountType(str, Enum):
+    CASH = "CASH"
+    MARGIN = "MARGIN"
+
 class AccountCashEquivalentType(str, Enum):
     SWEEP_VEHICLE = "SWEEP_VEHICLE"
     SAVINGS = "SAVINGS"
     MONEY_MARKET_FUND = "MONEY_MARKET_FUND"
     UNKNOWN = "UNKNOWN"
 
+class AccountOptionType(str, Enum):
+    VANILLA = "VANILLA"
+    BINARY = "BINARY"
+    BARRIER = "BARRIER"
+    UNKNOWN = "UNKNOWN"
 
+class APICurrencyType(str, Enum):
+    USD = "USD"
+    CAD = "CAD"
+    EUR = "EUR"
+    JPY = "JPY"
+
+class PutCall(str, Enum):
+    PUT = "PUT"
+    CALL = "CALL"
+    UNKNOWN = "UNKNOWN"
+
+class ExecutionType(str, Enum):
+    FILL = "FILL"
+
+class ActivityType(str, Enum):
+    EXECUTION = "EXECUTION"
+    ORDER_ACTION = "ORDER_ACTION"
+
+class OrderLegType(str, Enum):
+    EQUITY = "EQUITY"
+    OPTION = "OPTION"
+    INDEX = "INDEX"
+    MUTUAL_FUND = "MUTUAL_FUND"
+    CASH_EQUIVALENT = "CASH_EQUIVALENT"
+    FIXED_INCOME = "FIXED_INCOME"
+    CURRENCY = "CURRENCY"
+    COLLECTIVE_INVESTMENT = "COLLECTIVE_INVESTMENT"
+
+class PositionEffect(str, Enum):
+    OPENING = "OPENING"
+    CLOSING = "CLOSING"
+    AUTOMATIC = "AUTOMATIC"
+
+class QuantityType(str, Enum):
+    ALL_SHARES = "ALL_SHARES"
+    DOLLARS = "DOLLARS"
+    SHARES = "SHARES"
+
+class DivCapGains(str, Enum):
+    REINVEST = "REINVEST"
+    PAYOUT = "PAYOUT"
+
+class SecuritiesAccountBaseType(str, Enum):
+    CASH = "CASH"
+    MARGIN = "MARGIN"
+
+class ForexType(str, Enum):
+    STANDARD = "STANDARD"
+    NBBO = "NBBO"
+    UNKNOWN = "UNKNOWN"
+
+class FutureType(str, Enum):
+    STANDARD = "STANDARD"
+    UNKNOWN = "UNKNOWN"
+
+class TransactionCashEquivalentType(str, Enum):
+    SWEEP_VEHICLE = "SWEEP_VEHICLE"
+    SAVINGS = "SAVINGS"
+    MONEY_MARKET_FUND = "MONEY_MARKET_FUND"
+    UNKNOWN = "UNKNOWN"
